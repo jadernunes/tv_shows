7b6c9736a146ebe80de1e95f864f65ef2b7fd856
Jader Nunes <jadernunes.jbn@gmail.com>
2024-04-04 00:13:44 -0300

    Created loader

---
+++ tvshows/Components/LoaderView.swift
@@ -0,0 +1,16 @@
+//
+//  LoaderView.swift
+//  tvshows
+//
+//  Created by Jader Borba Nunes on 03/04/24.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    
+    var body: some View {
+        ProgressView()
+            .controlSize(.large)
+    }
+}
