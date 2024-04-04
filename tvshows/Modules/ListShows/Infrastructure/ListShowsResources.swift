5d35cb92b50701c72a24dad228cd45a44108380f
Jader Nunes <jadernunes.jbn@gmail.com>
2024-04-03 22:46:52 -0300

    Create service to request shows

---
+++ tvshows/Modules/ListShows/Infrastructure/ListShowsResources.swift
@@ -0,0 +1,38 @@
+//
+//  ListShowsResources.swift
+//  tvshows
+//
+//  Created by Jader Borba Nunes on 03/04/24.
+//
+
+enum ListShowsResources {
+    case loadAll(page: Int)
+}
+
+extension ListShowsResources: Requestable {
+    
+    var method: HTTPMethodType {
+        switch self {
+        case .loadAll:
+            return .get
+        }
+    }
+    
+    var parameters: Params? {
+        switch self {
+        case let .loadAll(page):
+            var params: [String: Any] = [
+                "page": page
+            ]
+            
+            return params
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .loadAll:
+            return "/shows"
+        }
+    }
+}
---
+++ tvshows/Modules/ListShows/Infrastructure/Models/ShowDTO.swift
@@ -0,0 +1,12 @@
+//
+//  ShowDTO.swift
+//  tvshows
+//
+//  Created by Jader Borba Nunes on 03/04/24.
+//
+
+import Foundation
+
+struct ShowDTO: Decodable {
+    let id: Int
+}
