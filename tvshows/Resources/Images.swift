485eb3ec3d7b2786f78cc4c41e29766d43908e9b
Jader Nunes <jadernunes.jbn@gmail.com>
2024-04-04 00:15:41 -0300

    Created default fonts and default images

---
+++ tvshows/Resources/Images.swift
@@ -0,0 +1,16 @@
+//
+//  Images.swift
+//  tvshows
+//
+//  Created by Jader Borba Nunes on 03/04/24.
+//
+
+import SwiftUI
+
+struct Images {
+    
+    struct NoImage {
+        static let uiImage = UIImage(named: "noImage") ?? .no
+        static let image = Image(uiImage: uiImage)
+    }
+}
