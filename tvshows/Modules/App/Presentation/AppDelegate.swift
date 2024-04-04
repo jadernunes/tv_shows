c2a3d0206ffa11c7bcc3c4129954458939b9d439
Jader Nunes <jadernunes.jbn@gmail.com>
2024-04-03 21:05:39 -0300

    Added start flow logic

---
+++ tvshows/Modules/App/Presentation/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  tvshows
+//
+//  Created by Jader Borba Nunes on 03/04/24.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+
+    // MARK: - UISceneSession Life cycle
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
