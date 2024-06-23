86dfaf68770ca6fb61a121b8d7863f3b9f674809
Jader Nunes <jadernunes.jbn@gmail.com>
2024-06-23 08:58:55 -0300

    Created localization module

---
+++ tvshows/libs/Localization/Tests/LocalizationTests/LocalizationTests.swift
@@ -0,0 +1,15 @@
+//
+//  LocalizationTests.swift
+//
+//
+//  Created by Jader Nunes on 22/06/24.
+//
+
+import XCTest
+@testable import Localization
+
+final class LocalizationTests: XCTestCase {
+    func testExample() throws {
+        // TODO: Created unit tests
+    }
+}
